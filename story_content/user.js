b6d43938fb004050bfc552a97579b532e78bec25
Azizah Fakhriyyah Zahra <azizahzahra753@gmail.com>
2026-03-31 23:23:08 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,172 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6STMoPBCp4Y":
+        Script1();
+        break;
+      case "5xJb6vML3QJ":
+        Script2();
+        break;
+      case "6dHrbr9lbhu":
+        Script3();
+        break;
+      case "6GNduX2GNFF":
+        Script4();
+        break;
+      case "6fwWKpGo1UN":
+        Script5();
+        break;
+      case "5zjPDRFDvCG":
+        Script6();
+        break;
+      case "6bhbbD8E2nH":
+        Script7();
+        break;
+      case "6R1sE89Q9WB":
+        Script8();
+        break;
+      case "6GnZBRCjbpW":
+        Script9();
+        break;
+      case "6rYy9oMXq61":
+        Script10();
+        break;
+      case "60gM9tnoYJV":
+        Script11();
+        break;
+      case "652EDeFwMwY":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+
+if (audio.paused) {
+    audio.play();
+} else {
+    audio.pause();
+}
+}
+
